cc6c495fbb0bd9ef91d8a5f7f8ffbe72cb906de1
saulo <sauloco022@gmail.com>
2023-09-15 15:59:13 -0300

    migration commit

---
+++ script.ts
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", onLoad);
+
+function onLoad(e: Event) {
+  initCanvas();
+}
+
+function initCanvas(){
+  const canvas = document.querySelector('canvas');
+
+  canvas.stroke
+}
